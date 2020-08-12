6ba7177b72868f772a17509ea232e29da6f3520e
Herculano Cunha <herculanocm@outlook.com>
2020-08-11 21:44:32 -0300

    initial

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+    name='cd-utils-logger',
+    version='1.0.0',
+    packages=setuptools.find_packages(),
+    url='',
+    license='MIT',
+    author='Herculano Cunha',
+    author_email='herculanocm@outlook.com',
+    description='Utilitário para Log',
+    keywords='tools utils logger',
+    install_requires=['colorama'],
+    python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
